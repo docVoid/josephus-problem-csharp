13a89ccf199aa044890551c8030dd6e0cd9aa09e
doc.void <81415427+docVoid@users.noreply.github.com>
2025-07-18 15:26:16 +0200

    Implement main logic for Josephus problem calculation

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿double potenz = 0;
+double potenzzaehler = 0;
+double n;
+double k;
+
+System.Console.Write("Gebe N ein: ");
+n = Int32.Parse(Console.ReadLine());
+
+while (potenz * 2 <= n)
+{
+    potenz = Math.Pow(2, potenzzaehler);
+    potenzzaehler++;
+}
+double z = n - potenz;
+
+k = 1 + 2 * z;
+System.Console.WriteLine($"K: {k}");
